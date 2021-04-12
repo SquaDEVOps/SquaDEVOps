d88f27623b826e0b8bbe7c94ebcdf5a38c96beb4
LucasHDSF <lucashiago.dsf@gmail.com>
2021-04-12 19:20:59 -0300

    CNAME added
    CNAME added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { translate } from './modules/translate/translate.js';
 import { GenerateSlide } from './modules/slider/slider.js';
 import { render_def } from './modules/render_defs/render-defs.js';
 import { WorkItems } from './modules/render/render.js';
@@ -5,18 +6,18 @@ import { renderAllRankings } from './modules/rankings/rankings.js';
 import { renderModal, activateModal } from './modules/modal/modal.js';
 import { listMenu } from './modules/menu-mobile/menu-mobile.js';
 import { routs } from './modules/routs/routs.js';
-import { translate } from './modules/translate/translate.js';
 import { makeAnchor } from './modules/anchor/anchor.js';
 import { scrollIt } from './modules/scroll/scroll.js';
 import { collapse } from './modules/collapse/collapse.js';
+
 
 const renderDef = render_def();
 const workItems = WorkItems();
 const renderRankings = renderAllRankings();
 const modal = renderModal();
+const activeTranslate = translate.getNewRequest();
 const mobileMenu = listMenu();
 const rout = routs();
-const activeTranslate = translate.getNewRequest();
 const scroll = scrollIt();
 const anchor = makeAnchor();
 const collpse = collapse();
